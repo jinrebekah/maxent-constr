Fix very important error in re_sigmas_xx calculation

optical_cond.py
@@ -23,10 +23,6 @@ import pickle
 from scipy.interpolate import CubicSpline
 default_figsize = plt.rcParams['figure.figsize']
 
-# Do sys thing
-# Actually restore the old "smoothed" alpha selection
-
-
 class sigma:
     def __init__(self, path=None, sigma_type=None, ws=None, dws=None, bs=0, settings_xx={}, settings_xy={}, pickle_file=None):
     # def __init__(self, path, sigma_type, ws, dws, bs=0, settings_xx = {}, settings_xy={}, pickle_file=None):
@@ -168,7 +164,6 @@ class sigma:
         if self.settings_xx['krnl'] == 'symm':
             # Symmetric krnl, with half tau and w range. Only unconstrained option
             g = self.chi_xx[resample, : self.L // 2 + 1] / chiq0w0 # when we truncate taus, it includes the midpoint
-            print('xx', g)
             A_xx, al_xx, As_xx, chi2s_xx = maxent.maxent(g, **self.input_xx) # No factor of 2 here
             # Fill in the negative w half of A_xx
             A_xx = np.concatenate((A_xx[::-1], A_xx))
@@ -185,7 +180,7 @@ class sigma:
                 self.input_xx['constr_matrix'] = B
                 self.input_xx['constr_vec'] = b
                 A_xx, al_xx, As_xx, chi2s_xx = maxent.maxent(g, **self.input_xx, inspect_al=inspect_al)
-        re_sigmas_xx = np.real(A_xx / self.dws * (chiq0w0 / self.sign.mean()) * np.pi)
+        re_sigmas_xx = np.real(A_xx / self.dws * (chiq0w0 / self.sign[resample].mean()) * np.pi)
         debug_vals = {'A_xx': A_xx, 'norm_xx': chiq0w0, 'al_xx': al_xx, 'As_xx': As_xx, 'chi2s_xx': chi2s_xx}
         return re_sigmas_xx, debug_vals
 
@@ -214,10 +209,10 @@ class sigma:
         f = np.append(self.chi_xx[resample].mean(0), self.chi_xx[resample].mean(0)[0]) - np.real(1j*np.append(self.chi_xy[resample].mean(0), -self.chi_xy[resample].mean(0)[0]))
         chiq0w0 = CubicSpline(self.taus, f).integrate(0, self.beta)
         g = (self.chi_xx[resample] - np.real(1j*self.chi_xy[resample])) / chiq0w0
-        print('here', g)
         if self.input_xy['opt_method'] == 'Bryan':
             # Unconstrained
             A_sum, al_sum, As_sum, chi2s_sum = maxent.maxent(g, **self.input_xy, inspect_al = inspect_al)
+            print("Does A_sum=A_xx", np.all(A_sum==A_xx))
         elif self.input_xy['opt_method'] == 'cvxpy':
             # Define symmetry constraint matrices
             b = 2*A_xx[self.N//2:]
@@ -226,6 +221,7 @@ class sigma:
             self.input_xy['constr_vec'] = b
             A_sum, al_sum, As_sum, chi2s_sum = maxent.maxent(g, **self.input_xy, inspect_al = inspect_al)
         sigmas_sum = np.real(A_sum / self.dws * (chiq0w0 / self.sign[resample].mean())) * np.pi
+        # np.real(A_xx / self.dws * (chiq0w0 / self.sign[resample].mean()) * np.pi)
         im_sigmas_xy = sigmas_sum-re_sigmas_xx
         # Kramer's Kronig for re_sigma_xy
         ys = CubicSpline(self.ws, im_sigmas_xy)(self.xs)
@@ -320,7 +316,7 @@ def plot_sigma(sig, ax, sigma_name, bs_idx=None, bs_mode='errorbar'):
         else:
             # Plot all bootstraps on top of each other
             for i in range(len(sig_bs)):
-                ax.scatter(ws, sig_bs[i], lw=1, color='#0C5DA5', alpha=0.9)
+                ax.plot(ws, sig_bs[i], lw=1, color='#0C5DA5', alpha=0.9)
     else:
         # Plot all bins result
         ax.plot(ws, sig.results[sigma_name][0])
@@ -372,6 +368,7 @@ def inspect_al(sig, sigma_type, bs, redo_select_al = False, als_plot=[]):
         chi2s = sig.results['chi2s_sum'][bs]
         sig_label = r'Im[$\sigma_{xy}(\omega)$]'
         als = sig.input_xy['als']
+
     als_plot.append(optimal_al) # always plot optimal al
 
     # Plot density plot of sigma vs. al, with neighboring plot of spectra at alpha slices in als_plot
@@ -410,14 +407,6 @@ def inspect_al(sig, sigma_type, bs, redo_select_al = False, als_plot=[]):
         ax[2].plot(sig.ws, sigmas_al[al_idx], color=color, label=rf'$\alpha$ = {al_plot: .2e}')
         for j in range(2): ax[j].axvline(al_plot, color=color) # Plot lines on colorplot and chi2 plots at als_plot
 
-    # Plot im_sig_xy
-    ax[2].plot(sig.ws, np.real((sig.results['A_sum'][bs])/sig.dws * (sig.results['norm_sum'][bs]/sig.sign[resample].mean()))*np.pi - sig.results['re_sig_xx'][bs])    #### THIS IS 0
-    print(als[al_idx], al_idx)
-    print(sig.results['A_sum'][bs]-sig.results['As_sum'][bs][al_idx])
-    
-        # if i == 1:
-        #     print(sigmas_al[al_idx]-sig.results['im_sig_xy'][bs])
-
     ax[2].set_xlabel(r'$\omega$')
     ax[2].set_ylabel(sig_label)
     ax[2].set_xlim(-20, 20)
@@ -425,7 +414,7 @@ def inspect_al(sig, sigma_type, bs, redo_select_al = False, als_plot=[]):
 
     plt.show()
 
-def compare_chi_tau(sigs, mode='xx'):
+def compare_chi_tau(sigs, mode='xx', bs=0):
     # Verify that sig1 and sig2 have the same data
     sig1 = sigs[0]
     taus = sig1.taus