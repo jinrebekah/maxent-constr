Critical update: add scaling between xx and xy pieces for off-diagonal part, which fixes issue when xy'' is actually large

my_my_maxent.py
@@ -61,7 +61,7 @@ def maxent(G, K, m, opt_method='Bryan', constr_matrix=None, constr_vec=None, smo
     G = np.dot(Uc, Gavg) # just calling these G and K
     
     # ------------------------------ Calculate Q, chi2, S, lnP, dlnP for all al in als ------------------------------
-    tol=1e-8
+    tol=1e-7
     
     N = K.shape[1]
     As = np.zeros((als.shape[0], N))
@@ -103,8 +103,8 @@ def maxent(G, K, m, opt_method='Bryan', constr_matrix=None, constr_vec=None, smo
         for i, al in enumerate(als):
             try:
                 alpha.value = al
-                Q_optimal = prob.solve(solver=cp.CLARABEL, verbose=True, warm_start=True, tol_feas=tol, tol_infeas_abs=tol, tol_infeas_rel=tol, tol_gap_abs=tol, tol_gap_rel=tol) # Probably more feasibility settings to be adjusted
-                Q_optimal = prob.solve(verbose=True, warm_start=True) # Probably more feasibility settings to be adjusted
+                Q_optimal = prob.solve(solver=cp.CLARABEL, verbose=False, warm_start=True, tol_feas=tol, tol_infeas_abs=tol, tol_infeas_rel=tol, tol_gap_abs=tol, tol_gap_rel=tol) # Probably more feasibility settings to be adjusted
+                # Q_optimal = prob.solve(verbose=True, warm_start=True) # Probably more feasibility settings to be adjusted
                 # Q_optimal = prob.solve(solver=cp.SCS, verbose=True, warm_start=True) # Probably more feasibility settings to be adjusted
                 As[i] = A.value
                 statuses[i] = prob.status
@@ -200,7 +200,7 @@ def select_al(als, As, Qs, Ss, chi2s, lnPs, dlnPs, statuses, al_method='BT', smo
     if inspect_al:
         # Report how many failed to solve
         print(f"Als failed to solve: {(~mask).sum()}")
-        print(als[~mask])
+        # print(als[~mask])
         if al is not None:
             print(f"Optimal chi2: {np.exp(chi2_fit(np.log(al)))}")
 

optical_cond.py
@@ -205,13 +205,15 @@ class sigma:
 
     def _calc_sigma_xy_bins(self, resample, inspect_al=False, return_As=False):
         """Calculates sigma_xy for bin indices specified by resample."""
+        xx_factor = 10
+    
         # Get sigma_xx
         re_sigmas_xx, debug_vals_xx = self._calc_sigma_xx_bins(resample, return_As=return_As)
         A_xx = debug_vals_xx['A_xx']
         # Maxent sum
-        f = np.append(self.chi_xx[resample].mean(0), self.chi_xx[resample].mean(0)[0]) - np.real(1j*np.append(self.chi_xy[resample].mean(0), -self.chi_xy[resample].mean(0)[0]))
+        f = xx_factor*np.append(self.chi_xx[resample].mean(0), self.chi_xx[resample].mean(0)[0]) - np.real(1j*np.append(self.chi_xy[resample].mean(0), -self.chi_xy[resample].mean(0)[0]))
         chiq0w0 = CubicSpline(self.taus, f).integrate(0, self.beta)
-        g = (self.chi_xx[resample] - np.real(1j*self.chi_xy[resample])) / chiq0w0
+        g = (xx_factor*self.chi_xx[resample] - np.real(1j*self.chi_xy[resample])) / chiq0w0
         if self.input_xy['opt_method'] == 'Bryan':
             # Unconstrained
             A_sum, al_sum, As_sum, chi2s_sum, lnPs_sum = maxent.maxent(g, **self.input_xy, inspect_al = inspect_al)
@@ -224,7 +226,7 @@ class sigma:
             A_sum, al_sum, As_sum, chi2s_sum, lnPs_sum = maxent.maxent(g, **self.input_xy, inspect_al = inspect_al)
         sigmas_sum = np.real(A_sum / self.dws * (chiq0w0 / self.sign[resample].mean())) * np.pi
         # np.real(A_xx / self.dws * (chiq0w0 / self.sign[resample].mean()) * np.pi)
-        im_sigmas_xy = sigmas_sum-re_sigmas_xx
+        im_sigmas_xy = sigmas_sum-xx_factor*re_sigmas_xx
         # Kramer's Kronig for re_sigma_xy
         ys = CubicSpline(self.ws, im_sigmas_xy)(self.xs)
         re_sigmas_xy = -np.imag(scipy.signal.hilbert(ys))
@@ -313,6 +315,7 @@ def calc_sig_xx_0(sig):
     return sig_xx_0, sig_xx_err
     
 def calc_sig_xy_0(sig):
+    re_sig_xy_bs = np.array(sig.results['re_sig_xy'].tolist())
     if nflux==0:
         sig_xy_0_bs = np.zeros_like(sig_xx_0_bs)
     else: